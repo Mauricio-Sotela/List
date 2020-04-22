ced42a2031311cf07029849555fbbfe41c04232d
sotela88 <j-ms-27@hotmail.com>
2020-04-22 14:10:34 +0200

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,14 @@
 let hide_menu= document.querySelector('.hide_menu');
 let left= document.querySelector('.left');
 let show_menu=document.querySelector('.show_menu');
-
+let new_list=document.querySelector('.new_list');
+let list_titles=document.querySelector('.list_titles');
+// let new_list=document.querySelector('.new_list');
+// let new_list=document.querySelector('.new_list');
+// let new_list=document.querySelector('.new_list');
+// let new_list=document.querySelector('.new_list');
+// let new_list=document.querySelector('.new_list');
+// let new_list=document.querySelector('.new_list');
 //hide menu
 hide_menu.addEventListener('click',()=>{
     left.classList.add('hide');
@@ -13,3 +20,11 @@ show_menu.addEventListener('click',()=>{
     left.classList.remove('hide');
     show_menu.classList.remove('show')
 });
+
+//add new list
+new_list.addEventListener('click',()=>{
+    let new_div=document.createElement('DIV');
+    new_div.setAttribute("class", "title");
+    new_div.innerHTML=`<h5>new</h5>`;
+    list_titles.appendChild(new_div);
+})
