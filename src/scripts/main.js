0c891434de7b6a7a350af6620cb1f43831910c4b
sotela88 <j-ms-27@hotmail.com>
2020-04-22 14:41:11 +0200

    list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,17 @@
+//  ELEMENTS
 let hide_menu= document.querySelector('.hide_menu');
 let left= document.querySelector('.left');
 let show_menu=document.querySelector('.show_menu');
 let new_list=document.querySelector('.new_list');
 let list_titles=document.querySelector('.list_titles');
-// let new_list=document.querySelector('.new_list');
-// let new_list=document.querySelector('.new_list');
-// let new_list=document.querySelector('.new_list');
-// let new_list=document.querySelector('.new_list');
-// let new_list=document.querySelector('.new_list');
-// let new_list=document.querySelector('.new_list');
+ let list_items =document.querySelector('.list_items');
+// let =document.querySelector('.');
+// let =document.querySelector('.');
+// let =document.querySelector('.');
+// let =document.querySelector('.');
+// let =document.querySelector('.');
+
+
 //hide menu
 hide_menu.addEventListener('click',()=>{
     left.classList.add('hide');
@@ -26,5 +29,21 @@ new_list.addEventListener('click',()=>{
     let new_div=document.createElement('DIV');
     new_div.setAttribute("class", "title");
     new_div.innerHTML=`<h5>new</h5>`;
-    list_titles.appendChild(new_div);
+    list_titles.insertBefore(new_div, list_titles.childNodes[0]);
+    
 })
+
+new_list.addEventListener('click',()=> {
+    // let userInput = document.querySelector(".list").value;
+    let new_div=document.createElement('DIV');
+    new_div.setAttribute("class", "title");
+    new_div.innerHTML=`<h5>new</h5>`;
+    list_items.insertBefore(new_div, list_items.childNodes[0]);
+
+
+    // let newLi = document.createElement("li");
+    // let text = document.createTextNode(userInput);
+    // newLi.appendChild(text);
+    // document.querySelector(".result").appendChild(newLi);
+    // document.querySelector("#userText").value = "";
+  });
