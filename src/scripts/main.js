02b40a86f6c259fbe5d6358d954c7fe3d1ac080e
sotela88 <j-ms-27@hotmail.com>
2020-04-22 17:22:13 +0200

    list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,8 +5,8 @@ let show_menu = document.querySelector(".show_menu");
 let new_list = document.querySelector(".new_list");
 let list_titles = document.querySelector(".list_titles");
 let list_items = document.querySelector(".list_items");
-let new_task = document.querySelector(".new_task");
-// let =document.querySelector('.');
+let new_task = document.querySelectorAll(".task_button");
+let task=document.querySelector('.task');
 // let =document.querySelector('.');
 // let =document.querySelector('.');
 // let =document.querySelector('.');
@@ -35,16 +35,13 @@ new_list.addEventListener("click", () => {
 new_list.addEventListener("click", () => {
     let new_div = document.createElement("DIV");
     new_div.setAttribute("class", "title");
-    new_div.innerHTML = `<div class="list_parent">
+    new_div.innerHTML = `
     <input type="text" name="list" class="main_title" placeholder="Untitled">
 
-    <div class="tasks">
-        <button class="new_task">
-            <i class="fas fa-plus"></i>
-        </button>
-    </div>
     
-</div> `;
+    <div class="task">
+
+    </div>`;
     list_items.insertBefore(new_div, list_items.childNodes[0]);
     let focus = document.querySelectorAll(".main_title");
     focus[0].focus();
@@ -61,7 +58,21 @@ new_list.addEventListener("click", () => {
     });
     blur();
     new_div.focus();
+    // console.log(new_task);
 });
 
 
+
 ///new task
+ new_task[0].addEventListener('click',to_do);
+
+function to_do(){
+    let new_div = document.createElement("ul");
+    new_div.setAttribute("class", "lista");
+    new_div.innerHTML = `<li><input type="checkbox"> <input type="text" class="ts"></li>`;
+    task.insertBefore(new_div, task.childNodes[0]);
+    focus=document.querySelectorAll('.ts');
+    focus[0].focus();
+    console.log(focus);
+    
+}
