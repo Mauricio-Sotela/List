74fac82a1897c577c9752e2d14fc27c0ef7a0b88
sotela88 <j-ms-27@hotmail.com>
2020-04-24 02:04:55 +0200

    list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -54,8 +54,7 @@ new_list.addEventListener("click", () => {
 
    let new_div = document.createElement("DIV");
    new_div.setAttribute("class", "title");
-   new_div.innerHTML = `<div class="task"></div>
-   <input type="text" name="list" class="main_title" placeholder="Untitled">`;
+   new_div.innerHTML = `<input type="text" name="list" class="main_title" placeholder="Untitled">`;
     list_items[0].insertBefore(new_div, list_items[0].childNodes[0]);
 
    
@@ -86,51 +85,114 @@ new_list.addEventListener("click", () => {
     // console.log(new_task);
     list_titles = document.querySelectorAll(".list_titles");
     
-    
-   // console.log(list_items);
-   arr=list_titles[0]['children'];
-   console.log(arr);
-   let aa = Array.from(arr);
+    //right array
+    arr2=list_items[0]['children'];
+    let bb = Array.from(arr2);
+    console.log(bb);
+
+    //left array
+   arr1=list_titles[0]['children'];
+   let aa = Array.from(arr1);
    console.log(aa);
+   console.log(task);
    
-   for (let i = 0; i < aa.length; i++) {
+   
+   for (let i = 1; i < aa.length; i++) {
       aa[i].addEventListener('click',()=>{
-           aa[i].style.cssText='color:red;' 
-        })
+        aa[i].style.cssText='color:red;' 
+        });
+         } ;
+         for (let j = 1; j < aa.length; j++){
+            aa[j].addEventListener('click',()=>{
+                 bb[j].style.cssText='display:none;'
+                });
+
+            
+        }
+
+/////////////////////////////////////////////////////////////////
+
+       
+            new_task[0].addEventListener('click',to_do);
+       
+       function to_do(){
+       let task=document.querySelectorAll('.task');
+              
+           let new_div = document.createElement("ul");
+           new_div.setAttribute("class", "lista");
+       
+           new_div.innerHTML = `<li><input type="checkbox"> <input type="text" class="ts"></li>`;
+
+          // task=document.querySelector('.task');
+
+           for (let i = 0; i < task.length; i++) {
+               console.log(list_items[0]);
+               
+                console.log(task[i]);
+                console.log(task);
+                 task=task 
+               task[i].insertBefore(new_div, task[i].childNodes[i]);
+           focus=document.querySelectorAll('.ts');
+           focus[0].focus();
+          focus[0].addEventListener('blur',()=>{
+          
+           if(focus[0].value==""){
+               task[i].removeChild(task[i].childNodes[0]);
+             
+           }
+        
+       
+       }); 
+           }
+
+          
+           
          
-       } 
-});
+       }
+
+       ///////////////////////////////
+}); 
 
 
 
-///new task
+// ///new task
 
-if(new_task){
-     new_task[0].addEventListener('click',to_do);
-}
-function to_do(){
-let task=document.querySelectorAll('.task');
+
+
+
+
+
+
+
+
+
+
+// if(new_task){
+//      new_task[0].addEventListener('click',to_do);
+// }
+// function to_do(){
+// let task=document.querySelectorAll('.task');
         
         
-    if(task){
+//     if(task){
          
-    let new_div = document.createElement("ul");
-    new_div.setAttribute("class", "lista");
+//     let new_div = document.createElement("ul");
+//     new_div.setAttribute("class", "lista");
 
-    new_div.innerHTML = `<li><input type="checkbox"> <input type="text" class="ts"></li>`;
-    task[1].insertBefore(new_div, task[1].childNodes[0]);
-    focus=document.querySelectorAll('.ts');
-    focus[0].focus();
-   focus[0].addEventListener('blur',()=>{
+//     new_div.innerHTML = `<li><input type="checkbox"> <input type="text" class="ts"></li>`;
+//     task[1].insertBefore(new_div, task[1].childNodes[0]);
+//     focus=document.querySelectorAll('.ts');
+//     focus[0].focus();
+//    focus[0].addEventListener('blur',()=>{
    
-    if(focus[0].value==""){
-        task[1].removeChild(task[1].childNodes[0]);
+//     if(focus[0].value==""){
+//         task[1].removeChild(task[1].childNodes[0]);
       
-    }
-// console.log(focus);
+//     }
+// // console.log(focus);
 
-});
-    }
+// });
+//     }
   
-}
+// }
 
